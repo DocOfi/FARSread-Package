449b6fe4a83113d0c7ac3d7095611341b29596c0
DocOfi <eofilada@yahoo.com>
2018-01-04 16:01:50 +0800

    added testfars.R to testthat folder

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,12 +2,6 @@ library(testthat)
 library(FARSread)
 
 test_check("FARSread")
-expect_that(dim(fars_summarize_years(2015))[1], is_equivalent_to(12))
-throws_error(fars_map_state(0, 2013))
-throws_error(fars_map_state(3, 2013))
-throws_error(fars_map_state(7, 2013))
-throws_error(fars_map_state(14, 2013))
-throws_error(fars_map_state(57, 2013))
 
 
 
