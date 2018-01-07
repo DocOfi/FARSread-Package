294fe7c4cc4b69c27ad8f3b8890434a1de98bc06
DocOfi <eofilada@yahoo.com>
2018-01-07 10:27:55 +0800

    added more test

--- tests/testthat/testfars.R
+++ tests/testthat/testfars.R
@@ -1,5 +1,5 @@
-library(FARSread)
-###invalid State numbers
+context("tests on inputs")###invalid State numbers
+
 test_that("results in error", {
         throws_error(fars_map_state(0, 2013))
         throws_error(fars_map_state(3, 2013))
@@ -9,4 +9,12 @@ test_that("results in error", {
         throws_error(fars_map_state(57, "Twenty Thirteen"))
 })
 
+context("test on outputs")
 
+test_that("dimensions are correct", {
+        fars_2013 <- system.file("extdata", "accident_2013.csv.bz2", package = "FARSread")
+        acc_2013 <- fars_read(fars_2013)
+        expect_that(acc_2013, is_a("tbl_df"))
+        expect_that(nrow(acc_2013), is_more_than(0))
+        expect_that(ncol(acc_2013), is_more_than(0))
+})
