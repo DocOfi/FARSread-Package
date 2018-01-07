6fa298e7fd2b83f541882bbcc22fed6dd66ec926
DocOfi <eofilada@yahoo.com>
2018-01-07 14:22:14 +0800

    added description for package

---
+++ R/FARSread.R
@@ -0,0 +1,17 @@
+#' Facilitate summary and visualization of Fatality Analysis Reporting System (FARS) data
+#'
+#' FARSread package allows you to create monthly summary tables of accidents per year and
+#' create map plots using geospatial locationing. See the README on
+#' \href{https://github.com/DocOfi/FARSread-Package}{GitHub}
+#'
+#' The function you're likely to find most useful from \pkg{FARSread} is
+#' \code{\link{fars_map_state}} to visualize location of accidents per State.
+#' Please refer to the vignettes to see other functions available in the package.
+#'
+#' @docType package
+#'
+#' @name FARSread
+#'
+#' @importFrom magrittr %>%
+"_PACKAGE"
+#> [1] "_PACKAGE"
