54a70283bce7411d39466c14a13cf94f1e0c6871
DocOfi <eofilada@yahoo.com>
2018-01-07 16:36:19 +0800

    used file.copy file.remove to test file_map_state

--- tests/testthat/testfars.R
+++ tests/testthat/testfars.R
@@ -18,3 +18,19 @@ test_that("dimensions are correct", {
         expect_that(nrow(acc_2013), is_more_than(0))
         expect_that(ncol(acc_2013), is_more_than(0))
 })
+
+test_that("test output of fars_map_state", {
+        fars_2013 <- system.file("extdata", "accident_2013.csv.bz2", package = "FARSread")
+        file.copy(from = fars_2013, to = getwd())
+        Texas <- fars_map_state(48, 2013)
+        expect_null(Texas, "null")
+        file.remove("./accident_2013.csv.bz2")
+})
+
+test_that("fars_summarize_years gives a warning", {
+        fars_2013 <- system.file("extdata", "accident_2013.csv.bz2", package = "FARSread")
+        file.copy(from = fars_2013, to = getwd())
+        expect_that(fars_summarize_years(c(2013, "Twenty Sixteen")), gives_warning())
+        file.remove("./accident_2013.csv.bz2")
+})
+
